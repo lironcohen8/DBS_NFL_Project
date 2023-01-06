053d54874d077ccde6fae56091ae8b3a6ef0b999
Liron <lironcohen3@mail.tau.ac.il>
2023-01-06 13:24:14 +0200

    added utils

---
+++ Utils/table_count_check_query.sql
@@ -0,0 +1,15 @@
+SELECT COUNT(*) FROM games; # should be 3668 
+
+SELECT COUNT(*) FROM players; # should be 2616 
+
+SELECT COUNT(*) FROM stats; # should be 4192
+
+SELECT COUNT(*) FROM teams; # should be 1790
+
+SELECT COUNT(*) FROM venues; # should be 804
+
+SELECT COUNT(*) FROM  draft_picks; # should be 262
+ 
+SELECT COUNT(*) FROM draft_positions; # should be 28
+
+SELECT COUNT(*) FROM draft_teams; # should be 32
