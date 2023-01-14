429dc9fed49757f54c206189ef5897b279b2392b
Liron <lironcohen3@mail.tau.ac.il>
2023-01-14 13:20:07 +0200

    Update query_db.py

--- SRC/QUERIES-DB-SCRIPT/query_db.py
+++ SRC/QUERIES-DB-SCRIPT/query_db.py
@@ -83,7 +83,8 @@ class DBQuery:
     # query 3
     def get_team_stats_better_than_average(self, input_team_name):
 
-        query = f"""SELECT s1.team, s1.stat_name, s1.stat_value FROM lironcohen3.stats AS s1
+        query = f"""SELECT s1.team, s1.stat_name, s1.stat_value
+                    FROM lironcohen3.stats AS s1
                     WHERE
                         s1.team = "{input_team_name}"
                         AND s1.stat_value >= (SELECT AVG(s2.stat_value) AS average_value
