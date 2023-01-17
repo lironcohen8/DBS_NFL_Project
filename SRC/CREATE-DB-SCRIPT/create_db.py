1e684fb77d3c9649f03aa112644f94a92d767c0f
Liron <lironcohen3@mail.tau.ac.il>
2023-01-17 16:35:32 +0200

    Update create_db.py

--- SRC/CREATE-DB-SCRIPT/create_db.py
+++ SRC/CREATE-DB-SCRIPT/create_db.py
@@ -139,9 +139,8 @@ INDEXES['venues'] = (
 INDEXES['draft_picks_id'] = (
     "CREATE INDEX college_id_ind ON draft_picks(`college_id`)")
 
-# TODO: check index is working
 INDEXES['draft_picks_nfl_team'] = (
-    "CREATE INDEX college_id_ind ON draft_picks(`nfl_team`)")
+    "CREATE INDEX nfl_team_ind ON draft_picks(`nfl_team`)")
 
 INDEXES['draft_teams'] = (
     "CREATE INDEX location_ind ON draft_teams(`location`)")
